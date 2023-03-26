f37263f83c2107a7fb123a55a7f78a4eb7ea39b2
oThiagoSouza <souzaalvesthiago99@gmail.com>
2023-03-25 23:09:12 -0300

    first commit

---
+++ Gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const imagemin = require('gulp-imagemin')
+
+function styles(){
+    return gulp.src('./src/styles/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(gulp.dest('./dist/css'))
+}
+
+function imageCompress(){
+    return gulp.src('./src/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/img'))
+}
+
+exports.default = gulp.parallel(styles, imageCompress)
+
+exports.watch = ()=> {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+}
